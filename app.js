b69fb3bf6d741473c0f569ef3a4c8828b09e529a
Ayush <rishiustaad@gmail.com>
2023-11-16 15:35:48 +0530

    fixed minor bugs

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-import express from 'express';
-import userRouter from './routes/user.js'
-import homeRoute from './routes/homeRoute.js';
-import taskRouter from './routes/task.js';
-import { config } from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { errorMiddleWare } from './middlewares/error.js';
-import cors from 'cors'
+import express from "express";
+import userRouter from "./routes/user.js";
+import homeRoute from "./routes/homeRoute.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleWare } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 
 config({
-    path: './data/config.env'
+  path: "./data/config.env",
 });
 
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials: true
-}))
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-// Using Routes 
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/task', taskRouter);
-app.use('/',homeRoute);
+// Using Routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+app.use("/", homeRoute);
+
+server.get("*", (req, res) =>
+  res.sendFile(path.resolve("build", "index.html"))
+);
 
 app.use(errorMiddleWare);
-
-
--- data/database.js
+++ data/database.js
@@ -1,9 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URL,{
-    dbName:'backendAPI'
-}).then(() => {console.log('database connected')}).catch((e) => [
-    console.log(e)
-]);
-}
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      dbName: "backendAPI",
+    })
+    .then(() => {
+      console.log("database connected");
+    })
+    .catch((e) => [console.log(e)]);
+};
